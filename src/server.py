f581a9400b3e17e7245473db36aa52f4516538fd
doome1337 <dmitry.paramonov@gmail.com>
2020-12-08 00:05:53 -0500

    [Experiment] Intermediate Server Alert Functionality.

--- src/server.py
+++ src/server.py
@@ -3,7 +3,8 @@ from collections import defaultdict
 from utils import count 
 
 class BaseServer:
-    def __init__(self, *args):
+    def __init__(self, alert_func, *args):
+        self.alert = alert_func
         self.message_count = 0
 
     def receive_message(self, *args):
@@ -15,11 +16,11 @@ class BaseServer:
 
 class EchoServer(BaseServer):
     def _receive_message(self, message):
-        print(message)
+        self.alert(message)
 
 class ZeroErrorServer(BaseServer):
-    def __init__(self, key_funcs, attr_funcs, queries):
-        super().__init__()
+    def __init__(self, key_funcs, attr_funcs, queries, *args):
+        super().__init__(*args)
         self.key_funcs = key_funcs
         self.attr_funcs = attr_funcs
         self.queries = queries
@@ -33,12 +34,13 @@ class ZeroErrorServer(BaseServer):
             attr = af(packet)
             self.table[q.name][key][1].add(attr)
             if len(self.table[q.name][key][1]) > q.threshold and not self.table[q.name][key][0]:
+                self.alert(q, key)
                 print('Query "{}" hit threshold for key {}'.format(q.name, key)) # TODO: logging 
                 self.table[q.name][key][0] = True
 
 class PMPServer(BaseServer):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args):
+        super().__init__(*args)
         self.coupons = {}
 
     def _receive_message(self, msg):
@@ -50,6 +52,7 @@ class PMPServer(BaseServer):
         if key_val in self.coupons[q]:
             self.coupons[q][key_val][1][coupon] = True
             if count(self.coupons[q][key_val][1]) >= cq.n and not self.coupons[q][key_val][0]:
+                self.alert(q, key)
                 print('Query "{}" hit threshold for key {}'.format(cq.query_name, key_val))
                 self.coupons[q][key_val][0] = True
         else:
